c1308abd3b5175916715312b90e1b961b2a893ed
FATIMAH ASIRI <fatimahasiri@FATIMAHs-MacBook-Pro-2.local>
2025-05-11 05:01:34 +0300

    added signin-signup

--- main.js
+++ main.js
@@ -25,3 +25,27 @@
     window.addEventListener('resize', callbackfunc)
     window.addEventListener('scroll', callbackfunc)
 })()
+
+
+
+let wrapper = document.getElementById('wrapper');
+let loginLink = document.getElementById('login-link');
+let registerLink = document.getElementById('register-link');
+let btnPopup = document.getElementById('btnLogin-popup');
+let iconClose = document.getElementById('icon-close');
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+})
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+})
+
+btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');
+})
